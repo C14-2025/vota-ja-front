87698c0191e8b48d21a608813afd09709f4d6cd1
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-10-10 21:01:43 -0300

    feat: adding the buttom type to the project

---
+++ src/types/button.ts
@@ -0,0 +1,19 @@
+export type ButtonVariant =
+  | "primary"
+  | "secondary"
+  | "ghost"
+  | "outline"
+  | "success"
+  | "danger";
+
+export type ButtonSize = "small" | "medium" | "large";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  fullWidth?: boolean;
+  loading?: boolean;
+  leftIcon?: React.ComponentType<{ size?: number | string; color?: string }>;
+  rightIcon?: React.ComponentType<{ size?: number | string; color?: string }>;
+}
