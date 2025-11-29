1672d740b35d55dc8b327497708d6618e4898cb0
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-11-29 14:58:05 -0300

    feat: add protected routes and navigation

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ProtectedRoute } from './ProtectedRoute';
+import { LoginPage, HomePage, CreatePollPage, PollDetailsPage } from '../pages';
+
+export const AppRoutes: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/polls/create" element={<CreatePollPage />} />
+          <Route path="/polls/:id" element={<PollDetailsPage />} />
+        </Route>
+
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
