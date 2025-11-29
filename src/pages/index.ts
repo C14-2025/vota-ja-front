23f0562b9a68b05f901c0f5854c1c13b1ce39d35
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-11-29 20:52:53 -0300

    chore: export RegisterPage in pages index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export { LoginPage } from './Login';
+export { RegisterPage } from './Register';
 export { HomePage } from './Home';
 export { CreatePollPage } from './CreatePoll';
 export { PollDetailsPage } from './PollDetails';
