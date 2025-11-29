44e168e1a25c08df0ac2bd7a650da0251357dd1f
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-11-29 20:52:53 -0300

    feat: add register route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ProtectedRoute } from './ProtectedRoute';
-import { LoginPage, HomePage, CreatePollPage, PollDetailsPage } from '../pages';
+import {
+  LoginPage,
+  RegisterPage,
+  HomePage,
+  CreatePollPage,
+  PollDetailsPage,
+} from '../pages';
 
 export const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
 
         <Route element={<ProtectedRoute />}>
           <Route path="/home" element={<HomePage />} />
