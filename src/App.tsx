f679fe34fcc1adc9eb0f25fa4d4c10e685411ec2
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-11-30 14:01:56 -0300

    chore: updating toast imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { AppRoutes } from './routes';
 import { AuthProvider } from './contexts';
+import { AppToast } from './common/Toast';
 
 function App() {
   return (
     <AuthProvider>
+      <AppToast />
       <AppRoutes />
     </AuthProvider>
   );
